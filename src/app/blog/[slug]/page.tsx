04e6d02304b508e3ef249fee335ca1c6f32566e2
unknown <fatmasaleh567@gmail.com>
2024-10-28 02:48:44 +0300

    change

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,36 +1,27 @@
-'use client'
 
-import { fullBlog } from "@/app/lib/interface";
-import { client, urlFor } from "@/app/sanity/client";
+import {  getPost, urlFor } from "@/app/sanity/client";
 import Image from "next/image";
 
-async function getData(slug:string) {
-    const query = `*[_type == "blog" && slug.current== '${slug}' ]{
-  'currentSlug' : slug.current,
-    content,
-    title,
-    mainImage,
-}[0]`;
-
-const data:fullBlog = await client.fetch(query);
-return data;
-
+// Define the type for your page props
+interface PageProps {
+  params: Params;
 }
-type PageProps = {
-  params: {
-    slug: string;
-  };
-};
 
-export default async function Blog ({ params }: PageProps)  {
-    const data:fullBlog = await getData(params.slug);
+// Interface for Params
+interface Params {
+  slug: string;
+}
+
+// Define the Blog component with correct types
+export default async function Blog({ params }: { params: { slug: string } }) {
+  const data = await getPost(params.slug);
   return (
     <div className="mt-8">
-        <h1>
-            <span className="block text-center text-primary font-semibold tracking-wide uppercase"> john Mike - Blog</span>
-            <span className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">{data.title}</span>
-        </h1>
-        <Image src={urlFor(data.mainImage).url()} alt="image" width={800} height={800} priority className="rounded-lg mt-8 border" /> 
+      <h1>
+        <span className="block text-center text-primary font-semibold tracking-wide uppercase">John Mike - Blog</span>
+        <span className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">{data.title}</span>
+      </h1>
+      <Image src={urlFor(data.mainImage).url()} alt="image" width={800} height={800} priority className="rounded-lg mt-8 border" />
     </div>
-  )
+  );
 }
--- src/app/sanity/client.ts
+++ src/app/sanity/client.ts
@@ -14,3 +14,15 @@ const builder = imageUrlBuilder(client) ;
 export function urlFor (source: any) {
   return builder.image(source)
 }
+
+export const getPost = async (slug: string) => {
+  const query = `*[_type == "blog" && slug.current== '${slug}' ][0]{
+    'currentSlug' : slug.current,
+      content,
+      title,
+      mainImage,
+  }`;
+  
+  const data = await client.fetch(query);
+  return data;
+}
